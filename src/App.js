aeaaad7df1a3c1e77d700e2d298432d4957f3505
James Rushford Redi <rushford.james+redi@gmail.com>
2021-05-06 20:19:29 +0200

    app set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const COOL_NAMES = [
+  'james'
+]
+
 function App() {
+  const [name, setName] = useState('')
+  const [isCool, setIsCool] = useState(false)
+
+  useEffect(()=>{
+    setIsCool(
+      COOL_NAMES.includes(name.toLocaleLowerCase())
+      )
+  },[name])
+
+  const handleChange = (e) =>{
+    setName(e.target.value)
+  }  
+  
   return (
     <div className="App">
+      <h1>Who is Cool?</h1>
+      <input value={name} onChange={handleChange}/>
+      {name && <p>{name} is {!isCool && 'not'} cool</p>}
     </div>
   );
 }
